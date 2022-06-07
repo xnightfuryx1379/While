b81743c8445482152721cde12fef7a794a316e8b
High&Fury <84853668+xnightfuryx1379@users.noreply.github.com>
2022-06-07 20:34:52 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x=0;
+ int sum=0;
+    int n;
+
+cout<<"Give me a number=";
+cin>>n;
+while(x<=n)
+{
+cout<<x<<'\t';
+   sum=sum+x;
+x++;
+
+
+
+}
+cout<<"sum="<<sum;
+
+    return 0;
+}
